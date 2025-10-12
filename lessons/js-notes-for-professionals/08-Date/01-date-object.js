846777a0f8c274dbe939db5d19412516e16741b3
Kairo Naser <kaironaser100@gmail.com>
2025-10-12 09:11:53 +0300

    Added ch08 Date , 01-date-objects

---
+++ lessons/js-notes-for-professionals/08-Date/01-date-object.js
@@ -0,0 +1,5 @@
+//now
+var now = new Date()
+console.log(now)//2025-10-12T06:07:44.541Z
+console.log(now.toString())//Sun Oct 12 2025 09:07:54 GMT+0300 (East Africa Time)
+console.log(now.toTimeString())//09:09:52 GMT+0300 (East Africa Time)
