32420458d41733218e32bb21e3d12d3eb55d07db
Kairo Naser <kaironaser100@gmail.com>
2025-10-12 11:53:56 +0300

    Added to lessons day 05 loops,

---
+++ lessons/daily/day-05-loops/loops.js
@@ -0,0 +1,47 @@
+ //for loop
+ console.log("this is for loop \n" )
+ for (var i =0 ; i<10 ; i++){
+    console.log(i)
+ }
+
+ //while loop
+ console.log("this is while loop \n" )
+ var w = 10
+ while ( w>=0){
+    console.log( w)
+    w--
+ }
+
+ // do while loop
+ console.log("this is do while loop \n")
+ var d = 0
+ do {
+    console.log(d)
+    d++
+ }
+ while(d <5)
+  
+
+
+//break: it terminates loop immediately
+console.log("This is for loop using break")
+ for (let i = 1; i<=16 ; i++)
+ {
+    console.log(i)
+    if(i==13) break
+   
+ }
+
+
+ 
+
+//continue : it skip that number and then continue the loop
+console.log("This is for loop using continue")
+for (let i = 1; i<=16 ; i++)
+{
+    
+    if(i==7) continue
+   console.log(i)
+ 
+ 
+}
