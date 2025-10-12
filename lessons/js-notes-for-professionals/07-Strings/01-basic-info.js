ae1319cecc17a7088162564ae04c355de34b3ae1
Kairo Naser <kaironaser100@gmail.com>
2025-10-12 09:03:08 +0300

    Added ch07 String , 01-basic

---
+++ lessons/js-notes-for-professionals/07-Strings/01-basic-info.js
@@ -0,0 +1,20 @@
+//String in js can be enclosed with single quote or double quotes or backticks
+var great= 'Hello'
+var who= "World"
+var when =`Morning`
+
+//String can be created from other types using String()
+var intString = String(32) //"32"
+var boolString = String(true) //"true"
+
+//toString() can be used convert numbers, boolean or objects to String
+var stringInt = (567).toString()
+
+//it can be concatenating using plus(+) comma(,) , or using concat()
+//String can be concatenating with other types but it will change to string
+var var1= "Hey"
+var var2 = "What's up?"
+console.log(var1 +', '+ var2)
+// console.log(var1 ,', ',var2)
+
+
