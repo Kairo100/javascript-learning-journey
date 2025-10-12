e03aea326496cc5f33a6c75ebb0f215448a2dc37
Kairo Naser <kaironaser100@gmail.com>
2025-10-12 12:10:32 +0300

    added day 06 functions lessons

---
+++ lessons/daily/day-06-functions/functions.js
@@ -0,0 +1,21 @@
+//declaration
+function myFunction(){
+ console.log("Hello from function")
+}
+
+//calling
+myFunction()
+
+//parameters
+function funParamerter( x){
+    console.log(x)
+}
+//arguments
+funParamerter(9)
+
+// return type
+function numFun (num1 , num2 ){
+    return num1 * num2
+}
+console.log(numFun(9, 4))
+
