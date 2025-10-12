4892e8298179aceed5ff1029e0a7002eedd472ea
Kairo Naser <kaironaser100@gmail.com>
2025-10-12 08:47:21 +0300

    added types of , chapter 06

---
+++ lessons/js-notes-for-professionals/06-data-types-in-javascript/types-of.js
@@ -0,0 +1,15 @@
+//strings
+console.log(typeof("abc8"))
+
+//numbers
+console.log(typeof(42))
+
+//Bool
+console.log(typeof(true))
+
+//objects
+console.log(typeof({}),", ", typeof([]) ,", ", typeof(null))
+
+
+//function
+console.log(typeof(function(){}))
